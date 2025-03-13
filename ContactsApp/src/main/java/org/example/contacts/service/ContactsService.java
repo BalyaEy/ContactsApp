8ae5972ee8381d0f133d2b5dfc83d8700dde552d
Eyüp Boyacı <eyup.boyaci@hotmail.com>
2025-03-13 21:14:44 +0300

    implemented ContactsService class

--- ContactsApp/src/main/java/org/example/contacts/service/ContactsService.java
+++ ContactsApp/src/main/java/org/example/contacts/service/ContactsService.java
@@ -1,4 +1,35 @@
 package org.example.contacts.service;
 
+import org.example.contacts.model.Person;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class ContactsService {
+    private List<Person> persons;
+
+    public ContactsService(){
+        this.persons = new ArrayList<>();
+    }
+
+    public void addPerson(Person person){
+        persons.add(person);
+    }
+
+    public void removePerson(String number){
+        persons.removeIf(p -> p.getNumber().equals(number));
+    }
+
+    public Person findPersonByPhone(String number){
+        for (Person p : persons){
+            if (p.getNumber().equals(number)){
+                return p;
+            }
+        }
+        return null;
+    }
+
+    public List<Person> listAll(){
+        return persons;
+    }
 }
